2b0c226b19c506e10e215cb9f668978fd66ae9bc
AlanisFoz <alanis.foz.pr@gmail.com>
2022-01-23 19:21:19 -0300

    add routes

--- startUp.ts
+++ startUp.ts
@@ -1,12 +1,9 @@
-import 'reflect-metadata';
 import express, { Application, Request, Response } from 'express';
 import database from './infra/db';
-import { NewsController } from './controller/newsController';
-import { VideosController } from './controller/videosController';
-import { GaleriaController } from './controller/galeriaController';
 
-import { container } from 'tsyringe';
-import './shared/container';
+import newsRouter from './router/newsRouter';
+import videosRouter from './router/videosRouter';
+import galeriaRouter from './router/galeriaRouter';
 
 class StartUp {
     public app: Application;
@@ -17,39 +14,15 @@ class StartUp {
         this._db.createConnection();
         this.routes();
     }
-    
-    private news = container.resolve(NewsController);
-    private videos = container.resolve(VideosController);
-    private galeria = container.resolve(GaleriaController);
 
     routes() {
         this.app.route("/").get((req: Request, res: Response) => {
             return res.send({ versao: "0.0.2"});
         });
 
-        this.app.route("api/v1/news/:page/:qtd").get((req: Request, res: Response) => {
-            return this.news.get(req, res);
-        })
-
-        this.app.route("api/v1/news/:id").get((req: Request, res: Response) => {
-            return this.news.getById(req, res);
-        })
-
-        this.app.route("api/v1/galeria/:page/:qtd").get((req: Request, res: Response) => {
-            return this.galeria.get(req, res);
-        })
-
-        this.app.route("api/v1/galeria/:id").get((req: Request, res: Response) => {
-            return this.galeria.getById(req, res);
-        })
-
-        this.app.route("api/v1/videos/:page/:qtd").get((req: Request, res: Response) => {
-            return this.videos.get(req, res);
-        })
-
-        this.app.route("api/v1/videos/:id").get((req: Request, res: Response) => {
-            return this.videos.getById(req, res);
-        })
+        this.app.use("/", newsRouter);
+        this.app.use("/", videosRouter);
+        this.app.use("/", galeriaRouter);
     }
 }
 
